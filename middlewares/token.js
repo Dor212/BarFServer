15870ddbae2e29f9ed35af29653efb7eb319bda7
dor <dor@ut.com>
2025-07-20 10:58:17 +0300

    1.4

--- middlewares/token.js
+++ middlewares/token.js
@@ -1,12 +1,15 @@
 import { verifyToken } from "../services/authService.js";
 
 export const auth = (req, res, next) => {
-  const tokenFromClient = req.header("x-auth-token");
-  if (!tokenFromClient) {
+  const authHeader = req.header("Authorization");
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ error: "No token provided." });
   }
 
-  const user = verifyToken(tokenFromClient);
+  const token = authHeader.split(" ")[1];
+  const user = verifyToken(token);
+
   if (!user) {
     return res.status(401).json({ error: "Invalid token." });
   }
